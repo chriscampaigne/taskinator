039cf06ccbcfdb7e0d8d840df1bb6bed448b061c
chriscampaigne <campaignemc@gmail.com>
2022-06-13 18:01:41 -0400

    fixed refresh on submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,19 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() { 
+var createTaskHandler = function(event) { 
 
-    var listItemEl = document.createElement("li"); 
+  event.preventDefault();
 
-    listItemEl.className = "task-item"; 
+  var listItemEl = document.createElement("li");
 
-    listItemEl.textContent = "This is a new task."; 
+  listItemEl.className = "task-item";
 
-    tasksToDoEl.appendChild(listItemEl); 
-    
-    }; 
+  listItemEl.textContent = "This is a new task.";
+
+  tasksToDoEl.appendChild(listItemEl);
+
+}; 
   
-  buttonEl.addEventListener("click", createTaskHandler);
+    formEl.addEventListener("submit", createTaskHandler);
